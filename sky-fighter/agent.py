2285491b0f63405fc7c688291fd9e8e5aaadae44
Yiran Deng <Yiran Deng>
2016-12-03 17:29:43 -0800

    most of the time awesome, sometimes stupid

--- sky-fighter/agent.py
+++ sky-fighter/agent.py
@@ -8,14 +8,20 @@ from collections import Counter
 def scoreEvaluationFunction(currentGameState):
     pos = currentGameState.getPlayerPosition()
     enemies = currentGameState.getEnemies()
+    projs = currentGameState.getProjectiles()
     enemyPos = currentGameState.getEnemyPositions()
     projPos = currentGameState.getProjPositions()
 
     def getSquaredDistance(pos1, pos2):
         return (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
 
-    enemyPosDiff = [getSquaredDistance(pos, enemy) for enemy in enemyPos if enemy[1] < pos[1] + PLAYER_SIZE]
-    projPosDiff = [getSquaredDistance(pos, proj) for proj in projPos if proj[1] < pos[1] + PLAYER_SIZE]
+    def getManhattanDistance(pos1, pos2):
+    	return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+    # enemyPosDiff = [getSquaredDistance(pos, enemy) for enemy in enemyPos if enemy[1] < pos[1] + PLAYER_SIZE]
+    # projPosDiff = [getSquaredDistance(pos, proj) for proj in projPos if proj[1] < pos[1] + PLAYER_SIZE]
+    enemyPosDiff = [getSquaredDistance(pos, enemy) for enemy in enemyPos]
+    projPosDiff = [getSquaredDistance(pos, proj) for proj in projPos]
 
     # calculate the number of threats in a range centered at player's position
     closestEnemyWeight, closestProjWeight = -50, -100
@@ -34,7 +40,7 @@ def scoreEvaluationFunction(currentGameState):
     # punish the score if flying too wide
     xDeviationWeight, yDeviationWeight = -1.0 / 25, -1.0 / 50
     xDeviation = abs(pos[0] - (SCREEN_WIDTH - PLAYER_SIZE) / 2) ** 2
-    yDeviation = abs(int(0.85 * SCREEN_HEIGHT) - pos[1])
+    yDeviation = abs(int(0.6 * SCREEN_HEIGHT) - pos[1])
     distToCenterScore = int(xDeviationWeight * xDeviation) + int(yDeviationWeight * yDeviation)
 
     # current game score
@@ -49,7 +55,38 @@ def scoreEvaluationFunction(currentGameState):
         horizontalDist = [4 * SCREEN_WIDTH]
     horizontalScore = int(horizontalDistWeight * sum(horizontalDist))
 
-    totalScore = [gameScore, closestEnemyScore, closestProjScore, distToCenterScore, horizontalScore]
+    # divide the screen into 4 pieces
+    W, H = SCREEN_WIDTH, SCREEN_HEIGHT
+    topleft = [(0, 0), (W / 2, 0), (0, H / 2), (W / 2, H / 2)]
+    bottomright = [(W / 2, H / 2), (W, H / 2), (W / 2, H), (W, H)]
+    center = []
+    for i in range(len(topleft)):
+    	ctr = 0.5 * (bottomright[i][0] - topleft[i][0]), 0.8 * (bottomright[i][1] - topleft[i][1])
+    	center.append(ctr)
+    enemyCenterOffset = ENEMY_WIDTH / 2, ENEMY_HEIGHT / 2
+    projCenterOffset = PROJECTILE_SIZE / 2, PROJECTILE_SIZE / 2
+    count = [0] * len(topleft)
+    for enemy in enemies:
+    	x, y = enemy.x + ENEMY_WIDTH, enemy.y + ENEMY_HEIGHT
+    	for i in range(len(topleft)):
+    		xIn = x > topleft[i][0] and x <= bottomright[i][0]
+    		yIn = y > topleft[i][1] and y <= bottomright[i][1]
+    		if xIn and yIn:
+    			count[i] += 1
+    for proj in projs:
+    	x, y = proj.x + PROJECTILE_SIZE, proj.y + PROJECTILE_SIZE
+    	for i in range(len(topleft)):
+    		xIn = x > topleft[i][0] and x <= bottomright[i][0]
+    		yIn = y > topleft[i][1] and y <= bottomright[i][1]
+    		if xIn and yIn:
+    			count[i] += 1
+    chosenArea = min(enumerate(count))[0]
+    playerCenter = pos[0] + PLAYER_SIZE / 2, pos[1] + PLAYER_SIZE / 2
+    distToChosenArea = getManhattanDistance(center[chosenArea], playerCenter)
+    distToAreaScore = -int(0.25 * distToChosenArea)
+
+    totalScore = [gameScore, closestEnemyScore, closestProjScore, distToCenterScore, horizontalScore, distToAreaScore]
+    # totalScore = [gameScore, closestEnemyScore, closestProjScore, horizontalScore, distToAreaScore]
     return sum(totalScore)
 
 
@@ -144,26 +181,48 @@ def ultimateEvaluationFunction(currentGameState):
     player = currentGameState.getPlayerPosition()
     gameScore = currentGameState.getScore()
     pos = currentGameState.getPlayerPosition()
+    enemyPos = currentGameState.getEnemyPositions()
+    projPos = currentGameState.getProjPositions()
+
+    def getSquaredDistance(pos1, pos2):
+        return (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
 
     def getManhattanDistance(pos1, pos2):
     	return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
-    enemyScore = 0
-    epDiff = (PLAYER_SIZE + ENEMY_WIDTH) / 2, (PLAYER_SIZE + ENEMY_HEIGHT) / 2
-    for enemy in enemies:
-    	ePos = enemy.x, enemy.y
-    	if ePos[1] >= player[1]:
-    		continue
-    	v = enemy.speed_x, enemy.speed_y
-    	threshold = (SCREEN_HEIGHT - ePos[1]) / v[1]
-    	if v[0] != 0:
-    		thresholdX = (SCREEN_WIDTH - ePos[0]) / v[0] if v[0] > 0 else ePos[0] / -v[0]
-    		threshold = min(threshold, thresholdX)    	
-    	for t in range(threshold):
-    		newePos = ePos[0] + v[0] * t, ePos[1] + v[1] * t
-    		if abs(newePos[0] - player[0]) <= epDiff[0] and abs(newePos[1] - player[1]) <= epDiff[1]:
-    			enemyScore -= 1000 / getManhattanDistance(ePos, player)
-    			break
+    # enemyScore = 0
+    # epDiff = (PLAYER_SIZE + ENEMY_WIDTH) / 2, (PLAYER_SIZE + ENEMY_HEIGHT) / 2
+    # for enemy in enemies:
+    # 	ePos = enemy.x, enemy.y
+    # 	if ePos[1] >= player[1]:
+    # 		continue
+    # 	v = enemy.speed_x, enemy.speed_y
+    # 	threshold = (SCREEN_HEIGHT - ePos[1]) / v[1]
+    # 	if v[0] != 0:
+    # 		thresholdX = (SCREEN_WIDTH - ePos[0]) / v[0] if v[0] > 0 else ePos[0] / -v[0]
+    # 		threshold = min(threshold, thresholdX)    	
+    # 	for t in range(threshold):
+    # 		newePos = ePos[0] + v[0] * t, ePos[1] + v[1] * t
+    # 		if abs(newePos[0] - player[0]) <= epDiff[0] and abs(newePos[1] - player[1]) <= epDiff[1]:
+    # 			enemyScore -= 1000 / getManhattanDistance(ePos, player)
+    # 			break
+
+    enemyPosDiff = [getSquaredDistance(pos, enemy) for enemy in enemyPos]
+    projPosDiff = [getSquaredDistance(pos, proj) for proj in projPos]
+
+    # calculate the number of threats in a range centered at player's position
+    closestEnemyWeight, closestProjWeight = -50, -100
+    radius = 256
+    closestEnemy = 0
+    for diff in enemyPosDiff:
+        if diff < (2 * radius) ** 2:
+            closestEnemy += radius ** 2 / diff
+    closestProj = 0
+    for diff in projPosDiff:
+        if diff < (2 * radius) ** 2:
+            closestProj += radius ** 2 / diff
+    closestEnemyScore = closestEnemyWeight * closestEnemy
+    closestProjScore = closestProjWeight * closestProj
 
     # divide the screen into 4 pieces
     W, H = SCREEN_WIDTH, SCREEN_HEIGHT
@@ -175,6 +234,24 @@ def ultimateEvaluationFunction(currentGameState):
     	center.append(ctr)
     enemyCenterOffset = ENEMY_WIDTH / 2, ENEMY_HEIGHT / 2
     projCenterOffset = PROJECTILE_SIZE / 2, PROJECTILE_SIZE / 2
+    count = [0] * len(topleft)
+    for enemy in enemies:
+    	x, y = enemy.x + ENEMY_WIDTH, enemy.y + ENEMY_HEIGHT
+    	for i in range(len(topleft)):
+    		xIn = x > topleft[i][0] and x <= bottomright[i][0]
+    		yIn = y > topleft[i][1] and y <= bottomright[i][1]
+    		if xIn and yIn:
+    			count[i] += 1
+    for proj in projs:
+    	x, y = proj.x + PROJECTILE_SIZE, proj.y + PROJECTILE_SIZE
+    	for i in range(len(topleft)):
+    		xIn = x > topleft[i][0] and x <= bottomright[i][0]
+    		yIn = y > topleft[i][1] and y <= bottomright[i][1]
+    		if xIn and yIn:
+    			count[i] += 1
+    chosenArea = min(enumerate(count))[0]
+    distToChosenArea = getManhattanDistance(center[chosenArea], pos)
+    distToAreaScore = -int(distToChosenArea)
 
     # punish the score if flying too wide
     xDeviationWeight, yDeviationWeight = -1.0 / 25, -1.0 / 50
@@ -182,7 +259,8 @@ def ultimateEvaluationFunction(currentGameState):
     yDeviation = abs(int(0.85 * SCREEN_HEIGHT) - pos[1])
     distToCenterScore = int(xDeviationWeight * xDeviation) + int(yDeviationWeight * yDeviation)
 
-    totalScore = [gameScore, enemyScore, distToCenterScore]
+    # totalScore = [gameScore]
+    totalScore = [gameScore, distToAreaScore, closestEnemyScore, closestProjScore]
     print totalScore
     return sum(totalScore)
 
@@ -192,18 +270,36 @@ def shootScoreEvaluationFunction(currentGameState):
     pos = currentGameState.getPlayerPosition()
     missilePos = currentGameState.getMissilePositions()
     enemies = currentGameState.getEnemies()
+    projs = currentGameState.getProjectiles()
     numMissile = len(missilePos)
     numEnemy = len(enemyPos)
 
+    playerCenter = pos[0] + PLAYER_SIZE / 2, pos[1] + PLAYER_SIZE / 2
+
+    # missileScore = 0
+    # for proj in projs:
+    # 	px, py = proj.x + PROJECTILE_SIZE / 2 + proj.speed_x, proj.y + PROJECTILE_SIZE / 2 + proj.speed_y
+    # 	diff = PLAYER_SPEED + PLAYER_SIZE / 2
+    # 	if abs(playerCenter[0] - px) < diff + PROJECTILE_SIZE / 2 and abs(playerCenter[1] - py) < diff + ENEMY_HEIGHT / 2:
+    # 		missileScore = -10000
+    # 		break
+    # for enemy in enemies:
+    # 	ex, ey = enemy.x + ENEMY_HEIGHT / 2 + enemy.speed_x, enemy.y + ENEMY_WIDTH / 2 + enemy.speed_y
+    # 	diff = PLAYER_SPEED + PLAYER_SIZE / 2
+    # 	if abs(playerCenter[0] - ex) < diff + ENEMY_WIDTH / 2 and abs(playerCenter[1] - ey) < diff + ENEMY_HEIGHT / 2:
+    # 		missileScore = -10000
+    # 		break
+
     hitOffset = MISSILE_WIDTH / 2, MISSILE_HEIGHT / 2
+    # hitOffset = 0, 0
     if numMissile > 0 or pos[1] < 0.8 * SCREEN_HEIGHT:
     	missileScore = -10000
     else:
     	missileScore = -10000
-    	mx, my = pos[0] + PLAYER_SIZE / 2, pos[1] + PLAYER_SIZE / 2
+    	mx, my = playerCenter
     	for enemy in enemies:
 	    	x, y = enemy.x + ENEMY_WIDTH / 2, enemy.y + ENEMY_HEIGHT / 2
-	    	if y >= my:
+	    	if y < 0 or y > my - PLAYER_SIZE / 2 - ENEMY_HEIGHT / 2:
 	    		continue
 	        vx, vy = enemy.speed_x, enemy.speed_y
 	        for t in range(1, my / MISSILE_SPEED):
@@ -212,7 +308,7 @@ def shootScoreEvaluationFunction(currentGameState):
 	            checkX = abs(newEnemyPos[0] - newMislePos[0]) < (ENEMY_WIDTH + MISSILE_WIDTH) / 2 - hitOffset[0]
 	            checkY = abs(newEnemyPos[1] - newMislePos[1]) < (ENEMY_HEIGHT + MISSILE_HEIGHT) / 2 - hitOffset[1]
 	            if checkX and checkY:
-	                missileScore = 200
+	                missileScore = 10000
 	                break
 	    # missileScore = -10000
 	    # rx = (ENEMY_WIDTH + MISSILE_WIDTH) / 2
@@ -331,7 +427,6 @@ class AlphaBetaAgent(Agent):
             indices = [i for i in range(len(choices)) if choices[i][0] == chosenValue[0]]
             chosenIndex = random.choice(indices)  # Pick randomly among max
             action = legalActions[chosenIndex]
-            print choices
             return chosenValue[0], action
 
         value, action = recurse(gameState, self.index, self.depth, -INF, INF)
@@ -342,10 +437,11 @@ class ExpectimaxAgent(Agent):
     def getAction(self, gameState):
         def recurse(state, index, depth):
             # check if it's the terminal state
+            choices = [Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT]
             if state.isWin() or state.isLose():
-                return state.getScore(), Directions.STOP
+                return state.getScore(), random.choice(choices)
             if len(state.getLegalActions(index)) == 0 or depth == 0:
-                return self.evaluationFunction(state), Directions.STOP
+                return self.evaluationFunction(state), random.choice(choices)
 
             nextIndex = (index + 1) % state.getNumAgents()
             nextDepth = depth - 1 if nextIndex == state.getNumAgents() - 1 else depth
